45bcae3ddcfbc37cbd35375cac0b20a314dbf3b9
Aamirrkhan <aamirkhan5156yusafzai@gmail.com>
2024-01-14 03:07:18 +0500

    player page start

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,41 @@
-const tabs = document.querySelectorAll('[data-tab-target]')
-const tabContents = document.querySelectorAll('[data-tab-content]')
+const tabs = document.querySelectorAll("[data-tab-target]");
+const tabContents = document.querySelectorAll("[data-tab-content]");
 
-tabs.forEach(tab => {
-  tab.addEventListener('click', () => {
-    const target = document.querySelector(tab.dataset.tabTarget)
-    tabContents.forEach(tabContent => {
-      tabContent.classList.remove('active')
-    })
-    tabs.forEach(tab => {
-      tab.classList.remove('active')
-    })
-    tab.classList.add('active')
-    target.classList.add('active')
-  })
-})
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.tabTarget);
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove("active");
+    });
+    tabs.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+    tab.classList.add("active");
+    target.classList.add("active");
+  });
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  function getRandomColor() {
+    var letters = "0123456789ABCDEF";
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  function setOrGetBoxColor(box, index) {
+    var storedColors = JSON.parse(localStorage.getItem("boxColors")) || {};
+    if (!storedColors[index]) {
+      storedColors[index] = getRandomColor();
+      localStorage.setItem("boxColors", JSON.stringify(storedColors));
+    }
+    box.style.backgroundColor = storedColors[index];
+  }
+  var playerBoxes = document.querySelectorAll(".player-box");
+  playerBoxes.forEach(function (box, index) {
+    setOrGetBoxColor(box, index);
+  });
+});
